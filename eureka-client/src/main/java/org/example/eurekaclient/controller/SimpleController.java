e0a4c4425f3acf3c1b18d62a67b642af87d1b8e5
GorbunovIvan <gorbunovvivan@gmail.com>
2024-01-05 13:40:50 +0200

    added an example of manually redirecting a request to the desired microservice.

--- eureka-client/src/main/java/org/example/eurekaclient/controller/SimpleController.java
+++ eureka-client/src/main/java/org/example/eurekaclient/controller/SimpleController.java
@@ -1,12 +1,14 @@
 package org.example.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/simple")
 public class SimpleController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
         return "test";
     }
