677e5fdc31ef5efab52bf8b9429e036d807695fa
GorbunovIvan <gorbunovvivan@gmail.com>
2024-01-05 14:14:23 +0200

    "instance-id" has been added to each microservice so that you can check the launch of several instances of the same microservice, thereby assessing the work of the load balancer

--- eureka-client/src/main/java/org/example/eurekaclient/controller/SimpleController.java
+++ eureka-client/src/main/java/org/example/eurekaclient/controller/SimpleController.java
@@ -1,5 +1,6 @@
 package org.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/simple")
 public class SimpleController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceID;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return "test (instance-id: '" + instanceID + "')";
     }
 }
